8898490e4b5df99aff0343eaef1703567859c13b
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-07-14 13:11:12 +0530

    Resolve Conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import SideBar from './components/SideBar'
 import { SidebarProvider } from './components/ui/sidebar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import TopBar from './components/TopBar'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
-    <div>
+    <div className='max-w-screen max-h-screen w-screen h-screen overflow-auto relative'>
       <TopBar />
-      <SideBar/>
-      <main className='text-5xl font-bold text-purple-600 flex items-center justify-center min-h-screen'>
-        BugBuster
-      </main>
+      <SidebarProvider>
+        <SideBar />
+      </SidebarProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route />
+          <Route />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
