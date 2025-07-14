b9b1872d7e326e76abd0214bb3465f48b869fafe
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-07-14 07:53:36 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-5xl font-bold text-purple-600'>
+        BugBuster
+      </h1>
+    </div>
+  )
+}
+
+export default App
