13950951eabe5d180d35b79d6dad5d7acf86c35a
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-07-14 11:43:32 +0530

    include TopBar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import TopBar from './components/TopBar'
+
 function App() {
   return (
     <div>
-      <h1 className='text-5xl font-bold text-purple-600'>
+      <TopBar />
+      <h1 className='text-5xl font-bold text-purple-600 flex items-center justify-center min-h-screen -translate-y-10'>
         BugBuster
       </h1>
     </div>
