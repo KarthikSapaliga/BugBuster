a41a4cfd7bcaaba696230f50f628e86e9a1cb042
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-07-14 13:12:34 +0530

    Add Dasboard component

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LineChart from '@/components/charts/LineChart'
+
+function Dashboard() {
+    return (
+        <div>
+            <LineChart />
+        </div>
+    )
+}
+
+export default Dashboard
