443867e1a375ccb5a703dc087636221c09f96480
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-07-14 20:15:50 +0530

    protect the routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,69 @@
-import React, { useEffect } from "react";
-
+import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import Layout from "./components/Layout";
+import { SignIn, SignUp } from "@clerk/clerk-react";
 
+import Layout from "./components/Layout";
+import Modal from "./components/Modal";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { Navigate } from "react-router-dom";
+
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/issues" element={<div>Issues Page</div>} />
-        <Route path="/project-settings" element={<div>Settings</div>} />
+        {/* Public Routes (in modal) */}
+        <Route
+          path="/signin"
+          element={
+            <Modal>
+              <SignIn signUpUrl="/signup" />
+            </Modal>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <Modal>
+              <SignUp signInUrl="/signin" />
+            </Modal>
+          }
+        />
+
+        {/* Protected Routes */}
+        <Route
+          path="*"
+          element={
+            <ProtectedRoute>
+              <Navigate to='/dashboard' />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/issues"
+          element={
+            <ProtectedRoute>
+              <div>Issues Page</div>
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/project-settings"
+          element={
+            <ProtectedRoute>
+              <div>Settings</div>
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Layout>
   );
