c3c32edab298614d36829c076eb448cb97b6902d
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-07-14 20:16:15 +0530

    wrap app with clerk provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,15 +3,23 @@ import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
 import { ThemeProvider } from './contexts/ThemeContext'
+import { ClerkProvider } from '@clerk/clerk-react'
 
 import "./styles/tailwind.css"
 import "./styles/index.css"
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
 
 createRoot(document.getElementById('root')).render(
-  <ThemeProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ThemeProvider>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/signin'>
+    <ThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </ClerkProvider>
 )
