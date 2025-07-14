859578ed5cd1613c4170ddc06ef668147549127c
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-07-14 13:29:31 +0530

    refactor main component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,33 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ThemeProvider } from './contexts/ThemeContext'
+
+import TopBar from './components/TopBar'
+import Dashboard from './pages/Dashboard'
+import SideBar from './components/SideBar'
+import { SidebarProvider } from './components/ui/sidebar'
 
 import "./styles/tailwind.css"
 import "./styles/index.css"
-import App from './App.jsx'
-import { ThemeProvider } from './contexts/ThemeContext'
+
 
 createRoot(document.getElementById('root')).render(
   <ThemeProvider>
-    <App />
+    <div className='max-w-screen max-h-screen w-screen h-screen overflow-auto relative'>
+      <TopBar />
+
+      <BrowserRouter>
+        <SidebarProvider>
+          <SideBar />
+        </SidebarProvider>
+        <Routes>
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route />
+          <Route />
+        </Routes>
+      </BrowserRouter>
+    </div>
   </ThemeProvider>
 )
