41b2adfc4818d6371495230bc2a493f169455f59
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-07-14 20:18:41 +0530

    add clerk user button

--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -3,16 +3,18 @@ import { UserRound, Sun, Moon } from "lucide-react";
 import { useTheme } from "@/contexts/ThemeContext";
 import { useSidebar } from "./ui/sidebar";
 
+import { SignedIn, UserButton, SignInButton, SignedOut } from '@clerk/clerk-react'
+
 export default function TopBar() {
     const { theme, toggleTheme } = useTheme();
-    const { toggleSidebar } = useSidebar(); 
+    const { toggleSidebar } = useSidebar();
 
 
     return (
         <header className="flex items-center justify-between border-b border-border bg-background px-4 py-3 shadow-sm">
             <div className="flex items-center gap-2 text-xl font-bold text-foreground">
                 <span className="text-lg mr-4 cursor-pointer" onClick={toggleSidebar}>
-                    <FaBars  />
+                    <FaBars />
                 </span>
                 <span className="text-lg p-1 bg-primary text-primary-foreground rounded-sm">
                     <FaBug />
@@ -28,7 +30,12 @@ export default function TopBar() {
                     {theme === "light" ? <Moon size={16} /> : <Sun size={16} />}
                 </button>
                 <div className="w-8 h-8 rounded-full bg-muted flex items-center justify-center p-2 cursor-pointer">
-                    <UserRound size={16} />
+                    <SignedIn>
+                        <UserButton />
+                    </SignedIn>
+                    <SignedOut>
+                        <SignInButton />
+                    </SignedOut>
                 </div>
             </div>
         </header>
