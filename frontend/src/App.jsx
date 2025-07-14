746bddc9708dc3b763684bc1ff1d518cbc5f529e
GlenFonceca <glenfonceca015@gmail.com>
2025-07-14 15:26:19 +0530

    Modified App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,44 +1,21 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 
-import TopBar from './components/TopBar'
-import Dashboard from './pages/Dashboard'
-import SideBar from './components/SideBar'
-import { SidebarProvider } from './components/ui/sidebar'
+import Layout from "./components/Layout";
 
-import { useState } from 'react'
 
 function App() {
-
-    const [sidebarOpen, setSidebarOpen] = useState(true);
-
-    const toggleSidebar = () => {
-        if (sidebarOpen) {
-            setSidebarOpen(false)
-        } else {
-            setSidebarOpen(true)
-        }
-    }
-
-    return (
-        <div className="w-screen h-screen flex flex-col overflow-hidden">
-            <TopBar toggleSidebar={toggleSidebar} />
-            <div className="flex flex-1">
-                <SidebarProvider>
-                    <SideBar />
-                </SidebarProvider>
-
-                <main className="flex-1 overflow-auto bg-background">
-                    <Routes>
-                        <Route path="/dashboard" element={<Dashboard />} />
-                        <Route path="/issues" element={<div>Issues Page</div>} />
-                        <Route path="/project-settings" element={<div>Settings</div>} />
-                    </Routes>
-                </main>
-            </div>
-        </div>
-    );
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/issues" element={<div>Issues Page</div>} />
+        <Route path="/project-settings" element={<div>Settings</div>} />
+      </Routes>
+    </Layout>
+  );
 }
 
-export default App
+export default App;
