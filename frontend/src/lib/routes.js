7bf82115d3de7b790c4b4c7a20026256ac7ce6b8
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-07-17 20:00:20 +0530

    include project routes

--- frontend/src/lib/routes.js
+++ frontend/src/lib/routes.js
@@ -3,10 +3,13 @@ export const HOST = import.meta.env.VITE_SERVER_URL;
 export const SIGNIN_ROUTE = "/api/auth/signin";
 export const SIGNUP_ROUTE = "/api/auth/signup";
 
-export const GET_DEVELOPERS_ROUTE = "/api/users/developers"         // GET - get all developers
+export const GET_DEVELOPERS_ROUTE = "/api/users/developers"        
 export const GET_DEVELOPERS_AND_TESTERS_ROUTE = "/api/users/devs-and-testers"
 
+// Project Routes
 export const CREATE_PROJECT_ROUTE = "/api/projects/create-project";
+export const UPDATE_PROJECT_ROUTE = "/api/projects/update-project"
+export const GET_MY_PROJECTS_ROUTE = "/api/projects/my-projects";
 
 // Bug Routes
 export const CREATE_BUG_ROUTE = "/api/bugs";                        // POST - Create a new bug
