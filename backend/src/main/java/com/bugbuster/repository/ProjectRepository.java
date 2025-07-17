d9f5dd095ce18ea1bb18158286f8653725f97861
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-07-17 15:03:33 +0530

    update service controller and repository for fetching projects

--- backend/src/main/java/com/bugbuster/repository/ProjectRepository.java
+++ backend/src/main/java/com/bugbuster/repository/ProjectRepository.java
@@ -2,6 +2,8 @@ package com.bugbuster.repository;
 
 import com.bugbuster.model.Project;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
 
 public interface ProjectRepository extends MongoRepository<Project, String> {
+    List<Project> findByTeamMembersContaining(String userId);
 }
