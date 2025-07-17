19d1c04daf737b44b1419b2c2318bd7ef52073ec
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-07-17 22:46:25 +0530

    add code to log the axios request and response

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,27 @@
 import axios from "axios";
 import { HOST } from "./routes.js";
 
-export const apiClient = axios.create({
+const apiClient = axios.create({
     baseURL: HOST,
     headers: {
         "Content-Type": "application/json",
     },
 });
+
+// apiClient.interceptors.request.use((request) => {
+//     console.log("Axios Request:\n", request);
+//     return request;
+// });
+
+// apiClient.interceptors.response.use(
+//     (response) => {
+//         console.log("Axios Response:\n", response);
+//         return response;
+//     },
+//     (error) => {
+//         console.error("Axios Error Response:\n", error.response);
+//         return Promise.reject(error);
+//     }
+// );
+
+export { apiClient };
