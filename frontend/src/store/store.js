dde8cc27bdeb1d18fe3f9d1d6bcab1a80a81c465
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-07-17 13:12:20 +0530

    fix the typo

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { createAuthSlice } from "./authSlice";
+import { createAuthSlice } from "./AuthSlice.js";
 import { persist } from "zustand/middleware";
 
 export const useAppStore = create(
