4b353bad6289c30eb132b36de4a4b5dbf76ad165
GlenFonceca <glenfonceca015@gmail.com>
2025-07-18 19:39:10 +0530

    Update File

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -4,7 +4,7 @@ import { GET_USER_ROUTE } from "./routes";
 export async function getUserName({ userId }) {
   try {
     const res = await apiClient.get(`${GET_USER_ROUTE}/${userId}`);
-    console.log(res.data.name);
+    //console.log(res.data.name);
     return res.data.name;
   } catch (error) {
     console.error("Failed to fetch user:", error);
