995ff3f6805d4bf485d097f2a648c1a5f276ce5a
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-07-18 23:42:05 +0530

    update routes

--- frontend/src/lib/routes.js
+++ frontend/src/lib/routes.js
@@ -22,6 +22,7 @@ export const GET_PROJECT_BY_ID_ROUTE = "/api/projects/get-by-id";
 // Bug Routes
 export const CREATE_BUG_ROUTE = "/api/bugs";                        // POST - Create a new bug
 export const GET_ALL_BUGS_ROUTE = "/api/bugs";                      // GET - Get all bugs
+export const GET_ASSIGNED_BUGS_ROUTE = "/api/bugs/assigned";        // GET - Get assigned bugs
 
 export const GET_BUG_BY_ID_ROUTE = "/api/bugs";                     // GET - Append /:id when calling
 export const UPDATE_BUG_ROUTE = "/api/bugs";                        // PUT - Append /:id when calling
@@ -31,6 +32,7 @@ export const GET_BUGS_BY_PROJECT_ROUTE = "/api/bugs/project";       // GET - App
 export const GET_BUGS_ASSIGNED_TO_ROUTE = "/api/bugs/assignedTo";   // GET - Append /:userId
 export const GET_BUGS_CREATED_BY_ROUTE = "/api/bugs/createdBy";     // GET - Append /:userId
 
-export const CLOSE_BUG_ROUTE = "/api/bugs";                         // PATCH - Append /:id/close?closedBy=userId
-export const ASSIGN_BUG_ROUTE = "/api/bugs";                        // PATCH - Append /:id/assign?assignedTo=userId&assignedBy=userId
-export const START_WORKING_ROUTE = "/api/bugs/start-working"        // PATCH - Append /:id
+export const CLOSE_BUG_ROUTE = "/api/bugs/close";                   // PATCH - Append /:id/
+export const ASSIGN_BUG_ROUTE = "/api/bugs/assign";                 // PATCH - Append /:id/
+export const RESOLVE_BUG_ROUTE = "/api/bugs/resolve";               // PATCH - Append /:id/
+export const START_WORKING_ROUTE = "/api/bugs/start-working"        // PATCH - Append /:id/
