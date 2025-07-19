2b5e85d74251fc7cde478fd8d8077234555e2d60
GlenFonceca <glenfonceca015@gmail.com>
2025-07-19 15:08:16 +0530

    Added function

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,6 +1,15 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge"
+import { AlertCircle, CheckCircle, Clock, PlayCircle, XCircle } from "lucide-react";
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
+}
+
+export function formatDate(dateString) {
+  if(!dateString) return null;
+  return new Date(dateString).toLocaleString("en-IN", {
+    dateStyle: "medium",
+    timeStyle: "short",
+  });
 }
