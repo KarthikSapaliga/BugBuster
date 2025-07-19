696080728b3057089bf075dffcdfc519d425f2ee
GlenFonceca <glenfonceca015@gmail.com>
2025-07-19 23:40:25 +0530

    Changes header part

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -9,6 +9,7 @@ import { GET_ALL_BUGS_ROUTE, GET_MY_PROJECTS_ROUTE } from "@/lib/routes";
 import { apiClient } from "@/lib/axios";
 import { calculateBugStats } from "@/lib/analytics";
 import Stats from "@/components/Stats";
+import { Badge } from "@/components/ui/badge";
 
 function Dashboard() {
   const { user } = useAppStore();
@@ -21,27 +22,40 @@ function Dashboard() {
 
   return (
     <main className="p-4 md:p-8 lg:p-12 flex flex-col gap-6 bg-background">
-      <div className="pb-5 border-b flex flex-col md:flex-row justify-between items-center">
-        <div>
-          <h1 className="text-4xl font-bold mb-2">{user.name}</h1>
-          <div className="flex gap-2 items-center">
-            <p className="text-xs px-2 py-1 bg-purple-300/30 border border-purple-400/50 rounded-full">
-              {user.role}
-            </p>
-            <p className="text-xs px-2 py-1 bg-emerald-300/30 border border-emerald-400/50 rounded-full">
-              {user.email}
-            </p>
+      <div className="pb-5 border-b border-border flex flex-col md:flex-row justify-between items-start md:items-center gap-4">
+        <div className="flex items-start gap-4">
+          <div className="w-16 h-16 bg-primary text-primary-foreground rounded-full flex items-center justify-center font-bold text-3xl shadow-md">
+            {user.name
+              .split(" ")
+              .map((n) => n[0])
+              .join("")}
+          </div>
+          <div>
+            <h1 className="text-3xl font-bold mb-2 text-foreground">
+              {user.name}
+            </h1>
+            <div className="flex gap-2 items-center flex-wrap">
+              <Badge
+                variant="default"
+                className="bg-primary "
+              >
+                {user.role}
+              </Badge>
+              <Badge variant="outline">{user.email}</Badge>
+            </div>
           </div>
         </div>
         {user.role === "MANAGER" ? (
-          <Button onClick={createProject}>Create New Project</Button>
+          <Button onClick={createProject} size="lg">
+            Create New Project
+          </Button>
         ) : (
           <></>
         )}
       </div>
-        <div>
-            <Stats/>
-        </div>
+      <div>
+        <Stats />
+      </div>
       <div className="flex flex-col md:flex-row md:gap-4 lg:gap-6">
         <RecentBugs />
         <QuickActions />
