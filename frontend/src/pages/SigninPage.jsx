e6e60f33c51bd2c689d9dab6f180cc6efa8e81ec
GlenFonceca <glenfonceca015@gmail.com>
2025-07-20 15:30:26 +0530

    changed background

--- frontend/src/pages/SigninPage.jsx
+++ frontend/src/pages/SigninPage.jsx
@@ -65,7 +65,7 @@ export default function SignInPage() {
     };
 
     return (
-        <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-slate-50 to-slate-100 dark:from-slate-900 dark:to-slate-800">
+        <div className="flex items-center justify-center min-h-screen bg-background ">
             <Card className="w-full max-w-md shadow-lg">
                 <CardHeader className="space-y-1">
                     <CardTitle className="text-2xl font-bold text-center">
